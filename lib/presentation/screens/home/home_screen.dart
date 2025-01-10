986c6e3ab9411b7d56b871833ef5114ef92c3869
Brihan Penayo <brihan.penayos@gmail.com>
2025-01-09 23:53:33 -0300

    se agrega homesScreen y se agregan lista de botones para prueba

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgep_app/config/menuItems/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,8 +7,51 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      title: const Text('Flutter Widgets with Material3'),
-    ));
+      appBar: AppBar(
+        title: const Text('Flutter Widgets with Material3'),
+      ),
+      body: const _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        final menuItem = appMenuItems[index];
+        return _CustomListTile(menuItem: menuItem);
+      },
+      itemCount: appMenuItems.length,
+    );
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  const _CustomListTile({
+    required this.menuItem,
+  });
+
+  final MenuItems menuItem;
+
+  @override
+  Widget build(BuildContext context) {
+    final color = Theme.of(context).colorScheme;
+    return ListTile(
+      leading: Icon(menuItem.icon, color: color.primary),
+      trailing: Icon(
+        Icons.chevron_right,
+        color: color.primary,
+      ),
+      title: Text(menuItem.title),
+      subtitle: Text(menuItem.subTitle),
+      onTap: () {
+        // TODO: navegar a otras pantallas al presionar
+        // Navigator.pushNamed(context, menuItem.link);
+      },
+    );
   }
 }
