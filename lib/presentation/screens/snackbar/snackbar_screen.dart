4352cc046a1a5a0728503cc65bd3a97d689b2967
Brihan Penayo <brihan.penayos@gmail.com>
2025-01-14 21:19:00 -0300

    se agrega snackbar

--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -3,9 +3,58 @@ import 'package:flutter/material.dart';
 class SnackbarScreen extends StatelessWidget {
   static const String name = 'snackbar_screen';
   const SnackbarScreen({super.key});
+  void showSnackbar(
+      {required BuildContext context, String message = 'Hola Mundo'}) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        showCloseIcon: true,
+        action: SnackBarAction(
+          label: 'OK',
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Snackbar Screen'),
+      ),
+      body: const _SnackbarView(),
+      floatingActionButton: FloatingActionButton.extended(
+        label: const Text('Mostrar Snackbar'),
+        icon: const Icon(Icons.remove_red_eye_outlined),
+        onPressed: () {
+          showSnackbar(context: context, message: 'Mostrando snackbar');
+        },
+      ),
+    );
+  }
+}
+
+class _SnackbarView extends StatelessWidget {
+  const _SnackbarView();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        child: const Text('Mostrar Snackbar'),
+        onPressed: () {
+          final snackBar = SnackBar(
+            content: const Text('Hola Mundo'),
+            action: SnackBarAction(
+              label: 'Ok!',
+              onPressed: () {},
+            ),
+          );
+          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+        },
+      ),
+    );
   }
 }
